cad83661d7ccf5e6b6f2535ca7dc7ea9bed0347e
Alberto De Lucca <albertodelucca@gmail.com>
2016-06-29 19:13:02 -0430

    Solve exercise 5

---
+++ 05.rb
@@ -0,0 +1,14 @@
+print "factorial of 5 is:"
+print 5 * 4 * 3 * 2 * 1
+
+puts
+print "factorial of 6 is:"
+print 6 * 5 * 4 * 3 * 2 * 1
+
+puts
+print "factorial of 7 is:"
+print 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts
+print "factorial of 8 is:"
+print 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
