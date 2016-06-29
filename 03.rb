e9b5309278d2bb090491c28d3fc5ab6d2d2eb67d
Alberto De Lucca <albertodelucca@gmail.com>
2016-06-29 19:06:52 -0430

    Solve exercise 3

---
+++ 03.rb
@@ -0,0 +1,5 @@
+movies = {"the lion king" => "1975", "jurassic park" => "2342", "the redemption" => "1834" }
+
+puts movies["the lion king"]
+puts movies["jurassic park"]
+puts movies["the redemption"]
