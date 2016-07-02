601aad5caf9f539d4e9ad45398c179a83b28152d
Alberto De Lucca <albertodelucca@gmail.com>
2016-07-01 19:59:11 -0430

    Solve exercise 04 chapter 04

---
+++ 04/04-name.rb
@@ -0,0 +1,7 @@
+print "Please type your first name> "
+first_name = gets.chomp
+
+print "Please type your last name> "
+last_name = gets.chomp
+
+puts "Hey " + first_name + " " + last_name "!" + " " + "How are you?"
